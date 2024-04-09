72621a6a9478ae7b390d9f2e20be7a372f8657ac
CntrolldSbstnce <reginaldmgriffinjr@protonmail.ch>
2024-04-09 16:11:20 -0400

    all

--- index.js
+++ index.js
@@ -50,3 +50,62 @@ const questions = [
     },
   ];
   
+  function generateReadme(answers) {
+    const licenseBadge = answers.license !== 'None' ? `![License](https://img.shields.io/badge/license-${answers.license}-blue.svg)` : '';
+    return `
+  # ${answers.title}
+  
+  ${licenseBadge}
+  
+  ## Description
+  
+  ${answers.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  \`\`\`
+  ${answers.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${answers.usage}
+  
+  ## License
+  
+  This project is licensed under the ${answers.license} license.
+  
+  ## Contributing
+  
+  ${answers.contribution}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${answers.test}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.github}](https://github.com/${answers.github}/).
+  `;
+  }
+  
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) throw err;
+      console.log('Successfully wrote to README.md');
+    });
+  });
